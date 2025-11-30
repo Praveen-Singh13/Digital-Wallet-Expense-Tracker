f73fd87e0e15afa3d9b077612f15e628ec92047e
Ritesh <25LBCS3192@culkomail.in>
2025-11-30 20:18:49 +0530

    Added the read me file

--- README.md
+++ README.md
@@ -4,3 +4,45 @@ A C-based Digital Wallet and Expense Tracker with deposits, withdrawals, categor
 
 #Project team members
 Ritesh Vishwakarma
+Praveen Singh
+Shreya Jaiswal
+Harsh Vikram Singh
+
+
+Project No 53:
+Digital wallet and expense tracker
+---------------------------------------------------------------------------------------------------------------------
+Requirements:
+
+1.Store transaction records using pointers and structures
+
+a structure to hold type of transaction(income or expense), amount, category and merchant
+a pointer-structure of dynamic size to hold the transactions collectively
+
+2.Display the current balance and transactions
+
+use for loops to print the transactions and calculate total balance
+
+3.Apply functions for each operations
+
+addTransactions()  //inputs transaction details such as type, amount, etc.
+calculateBalance   //calculates total balance and also updates the balance after every transaction
+generateReport()   //generates monthly reports and alerts if spending is more than 75% of income
+
+4.Edge case
+
+prompts the user if balance is insufficient and exits the program.
+
+---------------------------------------------------------------------------------------------------------------------------
+
+Important:
+
+update the balance after every transaction as failing to do so can lead to code breaking.
+
+---------------------------------------------------------------------------------------------------------------------------
+
+contact me at riteshvish94@gmail.com for anything about this project.
+Will be grateful for your constructive criticisms.
+
+----------------------------------------------------------------------------------------------------------------------------
+                                                                           All rights reserved
--- wallet/functions.c
+++ wallet/functions.c
@@ -20,69 +20,55 @@ struct transaction_details
     struct timestamp t;
     char merchant[50];
 };
-struct transaction_details *list = NULL;
-static int transactionCount = 0;
-struct transaction_details newTransaction;
+struct transaction_details *list = NULL; // Dynamic array to hold transactions
+
+static int Count = 0; //no. of transactions
+
+struct transaction_details newTransaction;//to hold new transaction
+
 static float balance = 0.0;
 
-
+//-------------------Fuctions---------------------------------//
 
 void clearBuffer()
 {
     while(getchar() != '\n');
 }
+
+//-------------------File handling functions-------------------//
+
 void saveToFile() {
-    FILE *fp = fopen("transactions.txt", "w");
+    FILE *fp = fopen("transactions.txt","w");
     if (!fp) {
-        printf("Error opening file for writing!\n");
+        printf("File not opened!!\n");
         return;
     }
-
-    for (int i = 0; i < transactionCount; i++) {
-        fprintf(fp, "%s %.2f %s %d %d %d %d %d %s\n",
-                list[i].type,
-                list[i].amount,
-                list[i].category,
-                list[i].t.day,
-                list[i].t.month,
-                list[i].t.year,
-                list[i].t.hour,
-                list[i].t.minute,
-                list[i].merchant);
+    for (int i = 0; i <Count; i++) {
+        fprintf(fp, "%s %.2f %s %d %d %d %d %d %s\n",list[i].type,list[i].amount,list[i].category,list[i].t.day,list[i].t.month, list[i].t.year,list[i].t.hour,list[i].t.minute,list[i].merchant);
     }
-
     fclose(fp);
 }
 
+
 void loadFromFile() {
-    FILE *fp = fopen("transactions.txt", "r");
+    FILE *fp = fopen("transactions.txt","r");
     if (!fp) {
         return; // No file yet
     }
-
     while (!feof(fp)) {
         struct transaction_details temp;
-
-        if (fscanf(fp, "%s %f %s %d %d %d %d %d %s",
-                   temp.type,
-                   &temp.amount,
-                   temp.category,
-                   &temp.t.day,
-                   &temp.t.month,
-                   &temp.t.year,
-                   &temp.t.hour,
-                   &temp.t.minute,
-                   temp.merchant) == 9)
+        if (fscanf(fp,"%s %f %s %d %d %d %d %d %s",temp.type, &temp.amount, temp.category, &temp.t.day,&temp.t.month, &temp.t.year, &temp.t.hour, &temp.t.minute,temp.merchant)==9)
         {
-            list = realloc(list, sizeof(struct transaction_details) * (transactionCount + 1));
-            list[transactionCount] = temp;
-            transactionCount++;
+            list = realloc(list, sizeof(struct transaction_details) * (Count + 1));
+            list[Count] = temp;
+            Count++;
         }
     }
 
     fclose(fp);
 }
 
+//-------------------Required functions-------------------//
 
 int addTransaction()
 {
@@ -90,11 +76,13 @@ int addTransaction()
     printf("Enter transaction type (income/expense):\n");
     
     fgets(newTransaction.type, 50, stdin);
+
     newTransaction.type[strcspn(newTransaction.type, "\n")] = 0;
-    
 
     printf("Enter amount:\n ");
+
     scanf("%f", &newTransaction.amount);
+
      if(newTransaction.amount>balance&&strcmp(newTransaction.type, "expense") == 0)
     {
         printf("Warning: Insufficient balance for this expense.\n");
@@ -102,9 +90,13 @@ int addTransaction()
         printf("Transaction not Valid.\n");
         return 0;
     }
+
     clearBuffer();
+
     printf("Enter category:\n ");
+
     fgets(newTransaction.category, 50, stdin);
+
     newTransaction.category[strcspn(newTransaction.category, "\n")] = 0;
     
     printf("Enter date (DD MM YYYY):\n"); 
@@ -114,26 +106,25 @@ int addTransaction()
     printf("Enter time (HH MM):\n");
     
     scanf("%d %d", &newTransaction.t.hour, &newTransaction.t.minute);
+
     clearBuffer();
+
     printf("Enter merchant:\n");
+
     fgets(newTransaction.merchant, 50, stdin);
     
     //store the newTransaction in the TransactionList
-    
-    
-    list = realloc(list, sizeof(struct transaction_details) * (transactionCount + 1));
-    list[transactionCount] = newTransaction;
-    transactionCount++;
-    saveToFile();
-    printf("Transaction added successfully!\n");
+     list = realloc(list, sizeof(struct transaction_details) * (Count + 1));
+     list[Count] = newTransaction;
+     Count++;
+     saveToFile();
+     printf("Transaction completed!!\n");
 }
 
 void calculateBalance()
 {
     //calculate and display balance
-
-    
-    for(int i = 0; i < transactionCount; i++)
+    for(int i = 0; i < Count; i++)
     {
         if(strcmp(list[i].type, "income") == 0)
         {
@@ -147,17 +138,17 @@ void calculateBalance()
     printf("Current Balance: %.2f\n", balance);
 }
 
-    void viewTransactions()
-    {
+void viewTransactions()
+{
     printf("Transactions:\n");
 
-    for(int i = 0; i < transactionCount; i++)
+    for(int i = 0; i < Count; i++)
     {
-        printf("Type: %s, Amount: %.2f, Category: %s, Date: %02d/%02d/%04d, Time: %02d:%02d, Merchant: %s\n",
-               list[i].type, list[i].amount, list[i].category,
-               list[i].t.day, list[i].t.month, list[i].t.year,
-               list[i].t.hour, list[i].t.minute,
-               list[i].merchant);
+        printf("Type: %s, Amount: %.2f, Category: %s, Date: %d/%d/%d, Time: %d:%d, Merchant: %s\n",
+            list[i].type, list[i].amount, list[i].category,
+            list[i].t.day, list[i].t.month, list[i].t.year,
+            list[i].t.hour, list[i].t.minute,
+            list[i].merchant);
     }
    
 }
@@ -166,39 +157,39 @@ void generateReport()
 {
     //generate monthly reports
     int month, year;
-    printf("Enter month and year for the report (MM YYYY):\n");
-    scanf("%d %d", &month, &year);
+    printf("Enter month and year for the montly report(MM YYYY):\n");
+    scanf("%d%d", &month, &year);
 
-    float totalIncome = 0.0;
-    float totalExpense = 0.0;
-    printf("Transactions for %02d/%04d:\n", month, year);
-    for(int i = 0; i < transactionCount; i++)
+    float total_income = 0.0;
+    float total_expense = 0.0;
+    printf("Transactions for %d %d:\n",month,year);
+    for(int i = 0; i < Count; i++)
     {
         if(list[i].t.month == month && list[i].t.year == year)
         {
             printf("Type: %s, Amount: %.2f, Category: %s, Date: %02d/%02d/%04d, Time: %02d:%02d, Merchant: %s\n",
-                   list[i].type, list[i].amount, list[i].category,
-                   list[i].t.day, list[i].t.month, list[i].t.year,
-                   list[i].t.hour, list[i].t.minute,
-                   list[i].merchant);
-            if(strcmp(list[i].type, "income") == 0)
+                list[i].type, list[i].amount, list[i].category,
+                list[i].t.day, list[i].t.month, list[i].t.year,
+                list[i].t.hour, list[i].t.minute,
+                list[i].merchant);
+            if(strcmp(list[i].type,"income")== 0)
             {
-                totalIncome += list[i].amount;
+                total_income += list[i].amount;
             }
-            else if(strcmp(list[i].type, "expense") == 0)
+            else if(strcmp(list[i].type,"expense")== 0)
             {
-                totalExpense += list[i].amount;
+                total_expense += list[i].amount;
             }
         }
     }
-    printf("Total Income: %.2f\n", totalIncome);
-    printf("Total Expense: %.2f\n", totalExpense);
-    printf("Net Savings: %.2f\n", totalIncome - totalExpense);
+    printf("Monthly Income: %.2f\n", total_income);
+    printf("Monthly Expenses: %.2f\n", total_expense);
+    printf("Monthly Savings: %.2f\n", total_income - total_expense);
     
     //alert if expenses exceed 75% of income
-    if(totalExpense > (0.75 * totalIncome))
+    if(total_expense > (0.75 * total_income))
     {
-        printf("Alert: Your expenses exceeded your income this month!\n");
+        printf("Alert!\n Your monthly limit has been reached!!\n");
     }
     
 }
